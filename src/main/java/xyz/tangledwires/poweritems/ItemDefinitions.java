693423cb0a6e7566b0ff9a1866aafbc00f2aea00
TangledWiresYT <daniel.smith@grism.com>
2022-05-26 17:39:14 +0100

    Initial commit

---
+++ src/main/java/xyz/tangledwires/poweritems/ItemDefinitions.java
@@ -0,0 +1,112 @@
+package xyz.tangledwires.poweritems;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class  ItemDefinitions {
+	private String hitDamageValue;
+	private String itemRarityType;
+	private String internalName;
+	private String itemName;
+	private String itemMaterial;
+	ItemDefinitions(String internalName, String itemName, String itemMaterial, String damageValue, String rarity) {
+		setInternalName(internalName);
+		setHitDamageValue(damageValue);
+		setItemRarityType(rarity);
+		setItemMaterial(itemMaterial);
+		setItemName(itemName);
+		Material material = Material.matchMaterial(itemMaterial);
+		if (material == null) {
+			Bukkit.getServer().getLogger().severe("Tried to create an item with an unknown material!");
+		}
+		else {
+			ItemStack is = new ItemStack(material);
+			setName(is, itemName);
+			setCustomLore(is);
+			setDamageValue(is, damageValue);
+			for(Player player : Bukkit.getOnlinePlayers()) {
+				player.getInventory().addItem(is);
+			}
+		}
+	}
+	public ItemStack setName(ItemStack is, String name){
+        ItemMeta m = is.getItemMeta();
+        m.setDisplayName(name);
+        is.setItemMeta(m);
+        return is;
+    }
+	public ItemStack setCustomLore(ItemStack is) {
+		ItemMeta m = is.getItemMeta();
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add("§r§7Damage: " + "§c+" + getHitDamageValue());
+		lore.add("");
+		switch (itemRarityType) {
+			case "common" :
+				lore.add("§r§f§lCOMMON");
+				break;
+			case "uncommon" :
+				lore.add("§r§a§lUNCOMMON");
+				break;
+			case "rare" :
+				lore.add("§r§9§lRARE");
+				break;
+			case "epic" :
+				lore.add("§r§5§lEPIC");
+				break;
+			case "legendary" :
+				lore.add("§r§6§lLEGENDARY");
+				break;
+			case "mythic" :
+				lore.add("§r§d§lMYTHIC");
+				break;
+			default:
+				lore.add("UNKNOWN");
+				break;
+		}
+		m.setLore(lore);
+		is.setItemMeta(m);
+		return is;
+	}
+	public ItemStack setDamageValue(ItemStack is, String damageValue) {
+		int intDamageValue = Integer.parseInt(damageValue);
+		ItemMeta m = is.getItemMeta();
+		m.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("generic.attackDamage", intDamageValue, Operation.ADD_NUMBER));
+		m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		is.setItemMeta(m);
+		return is;
+	}
+	public String getInternalName() {
+		return internalName;
+	}
+	public void setInternalName(String internalName) {
+		this.internalName = internalName;
+	}
+	public String getItemRarityType() {
+		return itemRarityType;
+	}
+	public void setItemRarityType(String itemRarityType) {
+		this.itemRarityType = itemRarityType;
+	}
+	public String getHitDamageValue() {
+		return hitDamageValue;
+	}
+	public void setHitDamageValue(String hitDamageValue) {
+		this.hitDamageValue = hitDamageValue;
+	}
+	public String toString(String internalName, String name, String material, int damage, String rarity) {
+		return "\nInternalName: " + internalName + "\nName: " + name + "\nMaterial: " + material + "\nDamage: " + damage + "\nRarity: " + rarity + "\n";
+	}
+	public String getItemName() { return itemName; }
+	public void setItemName(String itemName) {this.hitDamageValue = hitDamageValue;}
+	public String getItemMaterial() { return itemMaterial; }
+	public void setItemMaterial(String itemMaterial) {this.itemMaterial = itemMaterial;}
+}
---
+++ src/main/java/xyz/tangledwires/poweritems/PowerItems.java
@@ -0,0 +1,49 @@
+package xyz.tangledwires.poweritems;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import xyz.tangledwires.poweritems.ItemDefinitions;
+import org.bukkit.configuration.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class PowerItems extends JavaPlugin {
+	@Override
+    public void onEnable() {
+		Bukkit.getServer().getLogger().info("Loaded PowerItems. Enjoy!");
+    }
+    
+    @Override
+    public void onDisable() {
+        Bukkit.getServer().getLogger().info("PowerItems Disabled, bye! Have a nice day.");
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("testitem")) { 
+    		@SuppressWarnings("unused")
+			ItemDefinitions testItem = new ItemDefinitions("testItem", "Test Item", "DIAMOND_SWORD", "50", "common");
+    		@SuppressWarnings("unused")
+			ItemDefinitions masterStaff = new ItemDefinitions("masterStaff", "§r§9Master Staff", "BLAZE_ROD", "500", "rare");
+    		return true;
+    	}
+    	else if (cmd.getName().equalsIgnoreCase("createitem")) {
+    		ItemDefinitions commandItem = new ItemDefinitions(args[0], args[1], args[2], args[3], args[4]);
+			saveItemData(commandItem);
+    		return true;
+    	}
+    	return false; 
+    }
+	public void saveItemData(ItemDefinitions createdItem) {
+		this.getConfig().set("items." + createdItem.getInternalName() + ".itemName", createdItem.getItemName());
+		this.getConfig().set("items." + createdItem.getInternalName() + ".itemMaterial", createdItem.getItemMaterial());
+		this.getConfig().set("items." + createdItem.getInternalName() + ".damageValue", createdItem.getHitDamageValue());
+		this.getConfig().set("items." + createdItem.getInternalName() + ".itemRarity", createdItem.getItemRarityType());
+		this.saveConfig();
+	}
+}
